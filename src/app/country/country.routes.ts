eedbe7901d4024ec5140d211c725db77069d45b1
Irán Pérez <iran_phantom@hotmail.com>
2025-08-30 14:06:15 -0600

    Footer, top-menu and routes

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutPageComponent } from './layouts/country-layout-page/country-layout-page.component';
 
 export const countryRoutes: Routes = [
 
   {
     path: '',
-    component: ByCapitalPageComponent
+    component: CountryLayoutPageComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   }
 ];
 

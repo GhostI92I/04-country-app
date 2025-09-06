fcd309683cbb832283ccd276fc354473be35ae2a
Irán Pérez <iran_phantom@hotmail.com>
2025-09-06 10:57:07 -0600

    Footer design

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,4 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent { }
+export class FooterComponent {
+  currentYear = () => new Date().getFullYear();
+ }

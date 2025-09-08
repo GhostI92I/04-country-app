a70faf465797dc0d6f4db64e4a83b90e7ec52d8e
Irán Pérez <iran_phantom@hotmail.com>
2025-09-08 13:07:05 -0600

    Automatic searches, Country and Capital Cache

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,4 +9,15 @@ import { ChangeDetectionStrategy, Component, input, output } from '@angular/core
 export class SearchInputComponent {
   placeholder = input('Search...');
   value = output<string>();
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, 500);
+    onCleanup(() => clearTimeout(timeout))
+  })
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import type { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, delay, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mapper/country.mapper';
 import type { ICountry } from '../interfaces/country.interface';
 
@@ -13,13 +13,27 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, ICountry[]>();
+  private queryCacheCountry = new Map<string, ICountry[]>();
 
 
+  /* Search by capital */
   searchByCapital(query: string): Observable<ICountry[]> {
     query = query.toLowerCase();
 
+    console.log(this.queryCacheCapital);
+
+    /*     console.log(`Emmiting value of: "${query}"`);
+        return of([]); Rebounce for automatic searches, without do many requests to the backend */
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query) ?? []) /* return of(this.queryCacheCapital.get(query)!) Could be 'cause is already verified */
+    }
+    console.log(`Fetching from backend with value of: "${query}"`);
+
+
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
       .pipe(map((resp) => CountryMapper.mapRESTCountryArrayToCountryArray(resp)),
+        tap((countries) => this.queryCacheCapital.set(query, countries)),
         catchError(error => {
           console.log('Error fetching ', error);
           return throwError(() => new Error(`Country not found with this capital: "${query}"`))
@@ -31,8 +45,16 @@ export class CountryService {
   searchByCountry(query: string): Observable<ICountry[]> {
     query = query.toLowerCase();
 
+    console.log(this.queryCacheCountry);
+
+
+    if (this.queryCacheCountry.has(query)) {
+      return of(this.queryCacheCountry.get(query) ?? [])
+    }
+
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
       .pipe(map((resp) => CountryMapper.mapRESTCountryArrayToCountryArray(resp)),
+        tap((countries) => this.queryCacheCountry.set(query, countries)),
         delay(2000),
         catchError(error => {
           console.log('Error fetching ', error);

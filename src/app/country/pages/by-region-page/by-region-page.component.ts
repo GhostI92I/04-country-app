758a68ad6c4c5a4ff9eee57e3bf6445372c32040
Irán Pérez <iran_phantom@hotmail.com>
2025-09-09 17:31:50 -0600

    Keep results by Region QueryParams

--- src/app/country/pages/by-region-page/by-region-page.component.ts
+++ src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, linkedSignal, signal } from '@angular/core';
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { IRegion } from '../../interfaces/region.interface';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { CountryService } from '../../services/country.service';
 import { of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+
+function validateQueryParam(queryParam: string): IRegion {
+  queryParam = queryParam.toLowerCase();
+
+  const validRegions: Record<string, IRegion> = {
+    africa: 'Africa',
+    americas: 'Americas',
+    asia: 'Asia',
+    europe: 'Europe',
+    oceania: 'Oceania',
+    antarctic: 'Antarctic',
+  };
+  return validRegions[queryParam] ?? 'Americas'
+}
 
 @Component({
   selector: 'app-by-region-page',
@@ -22,13 +37,21 @@ export class ByRegionPageComponent {
     'Oceania',
     'Antarctic',
   ];
+  ActivatedRoute = inject(ActivatedRoute);
+  router = inject(Router)
 
-  selectedRegion = signal<IRegion | null>(null);
+  queryParam = this.ActivatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
+  selectedRegion = linkedSignal<IRegion>(() => validateQueryParam(this.queryParam));
 
   countryResource = rxResource({
     request: () => ({ region: this.selectedRegion() }),
     loader: ({ request }) => {
       if (!request.region) return of([]);
+
+      this.router.navigate(['/country/by-region'], {
+        queryParams: { region: request.region }
+      })
 
       return this.CountryService.searchByRegion(request.region)
     }
--- src/app/country/pages/by-region-page/region-information/region-information.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-region-information',
-  imports: [],
-  templateUrl: './region-information.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class RegionInformationComponent { }
